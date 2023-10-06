859c566866252cc6f475b38adcd1ba73d687c24b
skial <skial.bainn@gmail.com>
2023-10-06 10:21:23 +0100

    Use output extension if it exists to determine transformation. defaults to html. Overwrite output file if it exists.

--- binary/parse_md/src/main.rs
+++ binary/parse_md/src/main.rs
@@ -1,14 +1,16 @@
+use std::ffi::OsStr;
 use std::fs::File;
 use std::io::Write;
 use std::sync::Arc;
 use std::path::Path;
 use anyhow::anyhow;
 use clap::Parser as CliParser;
-use serve_md_core::determine;
+use serve_md_core::generate_payload_from_path;
+use serve_md_core::formats::Payload as PayloadFormats;
 use serve_md_core::state::State as Cli;
-use core::result::Result;
+use anyhow::Result;
 
-fn main() -> Result<(), anyhow::Error> {
+fn main() -> Result<()> {
     let mut cli = Cli::parse();
     cli.load_config();
     cli.set_missing();
@@ -18,24 +20,32 @@ fn main() -> Result<(), anyhow::Error> {
 
     let state = Arc::new(cli);
 
-    // TODO consider creating/exposing `generate_payload_from_{file, buffer, string}`
-    // if I want to support stdin -> stdout, temporary input files would be needed...
-    // as the lib relies on file extensions to work.
     if let Some(p) = state.file.as_ref() {
-        match determine(p.to_string(), state.clone()) {
-            Result::Ok(payload) => {
-                let exists:Option<(&Path, bool)> = state.output.as_ref()
-                    .map(|s| Path::new(s))
-                    .map(|p| (p, p.exists()));
+        let context: Option<(&Path, bool)> = state.output.as_ref()
+            .map(Path::new)
+            .map(|p| (p, p.exists()));
+        let ext = context
+            .and_then(|(p, _)| p.extension())
+            .and_then(OsStr::to_str)
+            .and_then(|s| PayloadFormats::try_from(s).ok())
+            .or(Some(PayloadFormats::Html))
+            ;
+        let input = Path::new(p);
+        
+        let res = generate_payload_from_path(input, Arc::clone(&state))
+            .and_then(|payload| payload.into_response_for(&ext.unwrap()))
+            .and_then(|payload| 
+            aquire_output(context).map(|writer| (payload, writer))
+        );
 
-                if let Result::Ok(mut writer) = aquire_output(exists) {
-                    match writer.write(&payload[..]) {
-                        Result::Ok(_) => {
-                            return Result::Ok(())
-                        },
-                        Err(e) => {
-                            return Err(e.into())
-                        }
+        match res {
+            Result::Ok((payload, mut writer)) => {
+                match writer.write(&payload[..]) {
+                    Result::Ok(_) => {
+                        return Result::Ok(())
+                    },
+                    Err(e) => {
+                        return Err(e.into())
                     }
                 }
 
@@ -54,7 +64,7 @@ fn aquire_output(output:Option<(&Path, bool)>) -> Result<Box<dyn Write>, anyhow:
             Result::Ok(Box::new(std::io::stdout()))
         }
         Some((path, true)) => {
-            match File::open(path) {
+            match File::options().write(true).open(path) {
                 Result::Ok(file) => {
                     Result::Ok(Box::new(file))
                 }
