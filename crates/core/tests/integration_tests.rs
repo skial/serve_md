d4ad86f83dc297aeba481f098820720caeff3548
skial <skial.bainn@gmail.com>
2023-10-09 09:33:43 +0100

    Add indoc dev dep. Test html output of test.md.

--- crates/core/tests/integration_tests.rs
+++ crates/core/tests/integration_tests.rs
@@ -1,6 +1,5 @@
-use std::sync::Arc;
-
-use serve_md_core::{determine, state::State};
+use std::{sync::Arc, path::Path};
+use serve_md_core::{determine, state::State, generate_payload_from_path};
 
 #[test]
 fn test_determine_with_existing_file() {
@@ -61,4 +60,57 @@ fn test_determine_with_incorrect_extension() {
         }
     }
     assert!( false, "Should have returned an error." )
+}
+
+#[test]
+fn test_gen_payload_missing_file() {
+    use pretty_assertions::assert_eq;
+
+    let path = format!("{}\\resources\\who.xxx", env!("CARGO_MANIFEST_DIR"));
+    let v = generate_payload_from_path(Path::new(&path), Arc::new(State::default()));
+    dbg!(&v);
+    match v {
+        Ok(_) => {},
+        Err(e) => {
+            assert_eq!(
+                e.to_string(),
+                format!("Path {path} does not exist."),
+            );
+            return;
+        }
+    }
+    assert!( false, "Should have returned an error." )
+}
+
+#[test]
+fn test_gen_payload_from_path() {
+    use pretty_assertions::assert_eq;
+
+    let path = format!("{}\\resources\\test.md", env!("CARGO_MANIFEST_DIR"));
+    let v = generate_payload_from_path(Path::new(&path), Arc::new(State::default()));
+    dbg!(&v);
+    match v {
+        Ok(payload) => {
+            assert_eq!( 
+                payload.front_matter,
+                ""
+            );
+            assert_eq!(
+                payload.html,
+                indoc::indoc! {r#"<h1>Header 1!</h1>
+                <p>some text that should be informative</p>
+                <ul>
+                <li>list item 1</li>
+                <li>list item 2</li>
+                <li>list item 3</li>
+                </ul>
+                <h2>Header 2</h2>
+                <p>hello world</p>
+                "#}
+            )
+        },
+        Err(e) => {
+            assert!( false, "Should NEVER return an error. Error was {e}" );
+        }
+    }
 }
